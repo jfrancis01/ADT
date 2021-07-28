6d94fb5723e241663a6dea325126a243b5fd9c03
jfrancis01 <joelfrancis@rogers.com>
2021-07-27 23:11:57 -0400

    Tried but didn't work

--- src/org/code/CountAndSay.java
+++ src/org/code/CountAndSay.java
@@ -5,32 +5,35 @@ import java.util.LinkedHashMap;
 public class CountAndSay {
 	
 	public static void main(String argv[]) {
-		int n = 5;
+		int n = 2;
 		CountAndSay cs = new CountAndSay();
 		System.out.println(cs.countAndSay(n));
 	}
 	
 	public String countAndSay(int n) {
 		
-		String start = "1";
+		String start = "55555";
 		if(n == 1) return start;
-		StringBuilder sb = null;
-		
-		for(int i = 2; i <=n; i++) {
-			//use the HashMap to find the frequency of the characters
-			LinkedHashMap<Character, Integer> hmap = new LinkedHashMap<Character, Integer>();		
-			for(int j = 0; j < start.length(); j++) {
-				hmap.put(start.charAt(j), hmap.getOrDefault(start.charAt(j), 0) + 1);
+		StringBuilder sb = new StringBuilder("");
+		int count = 0;
+		int i = 0;
+		int j = i;
+		while(i <= start.length() - 1) {
+			j = i;
+			while(j <= start.length() - 1) {
+				if(start.charAt(i) == start.charAt(j)) {
+					count++;
+					j++;
+				}
+				else {
+					i = j;
+					sb.append(count);
+					sb.append(start.charAt(i));
+					break;
+				}
 			}
-			sb = new StringBuilder("");
-			for(Character key : hmap.keySet()) {
-				int num = hmap.get(key);
-				sb.append(num);
-				sb.append(key);
-			}
-			start =sb.toString();
 		}
-		
+		//after it exits this loop add the last character to the end
 		return sb.toString();
 	}
 }
