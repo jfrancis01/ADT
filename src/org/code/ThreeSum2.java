e7b617288dda0834f7834f82d4d06cf8c9ca9440
jfrancisPura <joel.francis@purajuniper.com>
2021-08-24 01:11:56 -0400

    Completed 3sum using set where triplets or integer list is allowed

--- src/org/code/ThreeSum2.java
+++ src/org/code/ThreeSum2.java
@@ -2,22 +2,23 @@ package org.code;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ThreeSum2 {
 	
-	List<List<Integer>> ans = new ArrayList<List<Integer>>();
-	
+	Set<List<Integer>> triplets = new HashSet<>();
 	public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums); //sort to remove duplicate solutions
-        for(int i  = 0; i< nums.length; i++){
-            find2Sum(i, nums);
+        for(int i = 0; i < nums.length; i++) {
+        	find2Sum(i, nums);
         }
-        return ans;
+        return new ArrayList<>(triplets);
     }
     public void find2Sum(int index, int[] nums){
         int target = -1 * nums[index];
-        int left = index;
+        int left = index + 1;
         int right = nums.length - 1;
         while(left < right ){
             if(nums[left] + nums[right] == target){
@@ -25,7 +26,7 @@ public class ThreeSum2 {
                 row.add(nums[left]);
                 row.add(nums[right]);
                 row.add(target * -1);
-                ans.add(row);
+                triplets.add(row);
                 break;
             }
             else if(nums[left] + nums[right] < target){
@@ -38,7 +39,12 @@ public class ThreeSum2 {
     }
 
 	public static void main(String[] args) {
-		
+		ThreeSum2 ts2 = new ThreeSum2();
+		int[] nums = {-1,0,1,2,-1,-4};
+		List<List<Integer>> ans = ts2.threeSum(nums);
+		for(int i = 0; i < ans.size(); i++) {
+			System.out.println(ans.get(i));
+		}
 	}
 
 }
