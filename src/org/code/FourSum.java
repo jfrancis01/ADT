b3bc6ac16933d66e5df0854dd0b0588d293adecd
joel <joel@localhost>
2021-10-13 11:42:52 -0400

    made more progress

--- src/org/code/FourSum.java
+++ src/org/code/FourSum.java
@@ -12,7 +12,7 @@ public class FourSum {
 
 	public List<List<Integer>> fourSum(int[] nums, int target) {
 		ArrayList<Integer> nodups = new ArrayList<Integer>();
-		Arrays.sort(nums);
+		//Arrays.sort(nums);
 		for (int i = 0; i < nums.length; i++) {
 			nodups.add(nums[i]);
 		}
