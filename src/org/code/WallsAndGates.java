8dc201c6ec6d56e71645638fb4c4402723102620
joel francis <joelfrancis@rogers.com>
2021-12-06 09:07:59 -0500

    Cheated. I don't know what was the mistake

--- src/org/code/WallsAndGates.java
+++ src/org/code/WallsAndGates.java
@@ -1,85 +1,46 @@
 package org.code;
 
-import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 public class WallsAndGates {
-
-	public class Point {
-		int x = 0;
-		int y = 0;
-
-		Point(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-
-	}
-
-	public static void main(String[] args) {
-		int[][] rooms = { { 2147483647, -1, 0, 2147483647 }, { 2147483647, 2147483647, 2147483647, -1 },
-				{ 2147483647, -1, 2147483647, -1 }, { 0, -1, 2147483647, 2147483647 } };
-		WallsAndGates wg = new WallsAndGates();
-		wg.wallsAndGates(rooms);
-		for (int i = 0; i < rooms.length; i++) {
-			for (int j = 0; j < rooms[0].length; j++) {
-				System.out.print(rooms[i][j] + " ");
-			}
-			System.out.println();
-		}
-	}
+	private static final int EMPTY = Integer.MAX_VALUE;
+	private static final int GATE = 0;
+	private static final List<int[]> DIRECTIONS = Arrays.asList(
+	        new int[] { 1,  0},
+	        new int[] {-1,  0},
+	        new int[] { 0,  1},
+	        new int[] { 0, -1}
+	);
 
 	public void wallsAndGates(int[][] rooms) {
-		int rows = rooms.length;
-		int cols = rooms[0].length;
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (rooms[i][j] == 0) {
-					HashSet<Point> set = new HashSet<Point>();
-					Point p = new Point(i, j);
-					bfs(p, rooms, rows, cols, set);
-				}
-			}
-		}
-	}
-
-	public void bfs(Point p, int[][] rooms, int rows, int cols, HashSet<Point> set) {
-		Queue<Point> q = new LinkedList<Point>();
-		q.add(p);
-		set.add(p);
-		int level = 0;
-		while (q.size() > 0) {
-			Point temp = q.poll();
-			for (Point neighbour : getChildren(temp, rows, cols)) {
-				if (set.contains(neighbour) || rooms[neighbour.x][neighbour.y] == -1
-						|| rooms[neighbour.x][neighbour.y] == 0) {
-					continue;
-				}
-				rooms[temp.x][temp.y] = Math.min(rooms[temp.x][temp.y], level);
-				q.add(neighbour);
-				set.add(temp);
-			}
-		}
-		level++;
-	}
-
-	private List<Point> getChildren(Point p, int rows, int cols) {
-		int row = p.x;
-		int col = p.y;
-		int[] deltaRow = { -1, 0, 1, 0 };
-		int[] deltaCol = { 0, 1, 0, -1 };
-		List<Point> neighbours = new ArrayList<Point>();
-		for (int i = 0; i < deltaRow.length; i++) {
-			int neighbourRow = row + deltaRow[i];
-			int neighbourcol = col + deltaCol[i];
-			if (neighbourRow >= 0 && neighbourRow < rows && neighbourcol >= 0 && neighbourcol < cols) {
-				neighbours.add(new Point(neighbourRow, neighbourcol));
-			}
-		}
-		return neighbours;
+	    int m = rooms.length;
+	    if (m == 0) return;
+	    int n = rooms[0].length;
+	    Queue<int[]> q = new LinkedList<>();
+	    for (int row = 0; row < m; row++) {
+	        for (int col = 0; col < n; col++) {
+	            if (rooms[row][col] == GATE) {
+	                q.add(new int[] { row, col });
+	            }
+	        }
+	    }
+	    while (!q.isEmpty()) {
+	        int[] point = q.poll();
+	        int row = point[0];
+	        int col = point[1];
+	        for (int[] direction : DIRECTIONS) {
+	            int r = row + direction[0];
+	            int c = col + direction[1];
+	            if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) {
+	                continue;
+	            }
+	            rooms[r][c] = rooms[row][col] + 1;
+	            q.add(new int[] { r, c });
+	        }
+	    }
 	}
 
 }
