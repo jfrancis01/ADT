cd6f815c158cfb3142f7cb1b5db5e98e133293c3
joel <joel@localhost>
2021-12-20 09:03:35 -0500

    Greedy method

--- src/org/code/ShortestWayToFormString.java
+++ src/org/code/ShortestWayToFormString.java
@@ -7,10 +7,32 @@ public class ShortestWayToFormString {
 	ArrayList<String> subs = new ArrayList<String>();
 	int min = Integer.MAX_VALUE;
 
+	/*
+	 * public int shortestWay(String source, String target) {
+	 * printSubsequences(source, "", 0); shortestWay(source, target, 0, "", 0);
+	 * return min; }
+	 */
+
 	public int shortestWay(String source, String target) {
-		printSubsequences(source, "", 0);
-		shortestWay(source, target, 0, "", 0);
-		return min;
+		int ans = 0;
+		int j = 0;
+		char[] t = target.toCharArray();
+		char[] s = source.toCharArray();
+		while (j < t.length) {
+			int i = 0;
+			boolean notFound = true;
+			while (i < s.length && j < t.length) {
+				if (s[i] == t[j]) {
+					j++;
+					notFound = false;
+				}
+				i++;
+			}
+			if (notFound)
+				return -1;
+			ans++;
+		}
+		return ans;
 	}
 
 	private void shortestWay(String source, String target, int start, String test, int count) {
@@ -19,7 +41,6 @@ public class ShortestWayToFormString {
 		}
 		if (test.length() == target.length()) {
 			if (test.equals(target)) {
-				System.out.println(test);
 				min = Math.min(min, count);
 			}
 			return;
@@ -31,7 +52,6 @@ public class ShortestWayToFormString {
 
 	private void printSubsequences(String str, String seq, int start) {
 		if (start >= str.length()) {
-			System.out.println(seq);
 			if(seq.isEmpty()||seq.isBlank()) return;
 			subs.add(seq);
 			return;
@@ -40,10 +60,10 @@ public class ShortestWayToFormString {
 		printSubsequences(str, seq + c, start + 1);
 		printSubsequences(str, seq, start + 1);
 	}
-	
+
 	public static void main(String[] args) {
 		ShortestWayToFormString shtway = new ShortestWayToFormString();
-		int min = shtway.shortestWay("xyz", "xzyxz");
+		int min = shtway.shortestWay("abc", "abcbc");
 		if(min == Integer.MAX_VALUE) System.out.println(-1);
 		else System.out.println(min);
 	}
