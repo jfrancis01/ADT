028ee5b3b1979d6adec928565901e9e4a53a7e43
joel <joel@localhost>
2021-12-21 21:04:06 -0500

    tricky

--- src/org/code/SlidingPuzzle.java
+++ src/org/code/SlidingPuzzle.java
@@ -1,72 +1,72 @@
 package org.code;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class SlidingPuzzle {
 	int moves = Integer.MAX_VALUE;
+	HashMap<String, Integer> visited = new HashMap<String, Integer>();
 	public int slidingPuzzle(int[][] board) {
 		int move  = 0;
-		boolean visited[][] = new boolean[board.length][board[0].length];
 		for(int i  = 0; i < board.length; i++) {
 			for(int j = 0; j < board[0].length; j++) {
 				if(board[i][j]== 0) {
-					slidingPuzzleHelper(board, move, i, j, visited);
+					slidingPuzzleHelper(board, move, i, j);
+					break;
 				}
 			}
 		}
 		return moves;
 	}
 	
-	private void slidingPuzzleHelper(int[][]board, int move, int i, int j, boolean visited[][]) {
-		printState(board);
+	private void slidingPuzzleHelper(int[][]board, int move, int i, int j) {
 		if(isValid(board)) {
 			moves = Math.min(moves, move);
 			return;
 		}
+		String hash = hash(board);
+		//if(isVisited(hash)) {
+		//	return;
+		//}
+		if(visited.getOrDefault(hash, Integer.MAX_VALUE) < move) return;
+		visited.put(hash, move);
 		int temp = -1;
-		visited [i][j] = true;
-		//up
-		if( i + 1 < board.length && i + 1 >= 0 && !visited[i + 1][j]) {
+		
+		if( i + 1 < board.length && i + 1 >= 0 && j>=0 && j < board[0].length) {
 			temp = board[i + 1][j];
 			board[i][j] = temp;
 			board[i + 1][j] = 0;
-			slidingPuzzleHelper(board, move + 1, i + 1, j, visited);
+			slidingPuzzleHelper(board, move + 1, i + 1, j);
 			board[i][j] = 0;
 			board[i + 1][j] = temp;
-			printState(board);
 		}
 		
-		if( i-1  < board.length && i -1  >= 0 && !visited[i-1][j]) {
+		if( i-1  < board.length && i -1  >= 0 &&  j>=0 && j < board[0].length ) {
 			temp = board[i - 1][j];
 			board[i][j] = temp;
 			board[i - 1][j] = 0;
-			slidingPuzzleHelper(board, move + 1, i - 1, j, visited);
+			slidingPuzzleHelper(board, move + 1, i - 1, j);
 			board[i][j] = 0;
 			board[i - 1][j] = temp;
-			printState(board);
 		}
 		
-		if( j + 1  < board[0].length && j + 1  >= 0 && !visited[i][j + 1]) {
+		if( j + 1  < board[0].length && j + 1  >= 0 && i>=0 && i < board.length) {
 			temp = board[i][j + 1];
 			board[i][j] = temp;
 			board[i][j + 1] = 0;
-			slidingPuzzleHelper(board, move + 1, i, j + 1, visited);
+			slidingPuzzleHelper(board, move + 1, i, j + 1);
 			board[i][j] = 0;
 			board[i][j + 1] = temp;
-			printState(board);
 		}
 		
-		if( j - 1  < board[0].length && j - 1  >= 0 && !visited[i][j- 1]) {
+		if( j - 1  < board[0].length && j - 1  >= 0 && i>=0 && i < board.length) {
 			temp = board[i][j - 1];
 			board[i][j] = temp;
 			board[i][j - 1] = 0;
-			slidingPuzzleHelper(board, move + 1, i, j - 1, visited);
+			slidingPuzzleHelper(board, move + 1, i, j - 1);
 			board[i][j] = 0;
 			board[i][j - 1] = temp;
-			printState(board);
 		}
-		
-		visited [i][j] = false;
 	}
 	
 	private boolean isValid(int[][] board) {
@@ -79,6 +79,16 @@ public class SlidingPuzzle {
 			}
 		}
 		return true;
+	}
+	
+	private String hash(int[][] board) {
+		StringBuilder sb = new StringBuilder("");
+		for(int i  = 0; i < board.length; i++) {
+			for(int j  = 0; j < board[0].length; j++) {
+				sb.append(""+ board[i][j]);
+			}
+		}
+		return sb.toString();
 	}
 	
 	private void printState(int[][] board) {
