45d0d8cc5bfc3db698105919e230e47fb6311242
joel francis <joelfrancis@rogers.com>
2021-12-28 22:45:14 -0500

    solved with heap

--- src/org/code/PathMaxProbaility.java
+++ src/org/code/PathMaxProbaility.java
@@ -2,16 +2,19 @@ package org.code;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
 
 public class PathMaxProbaility {
-	
+
 	HashMap<Integer, Double> dist = new HashMap<Integer, Double>();
-	
-	class Link{
+
+	class Link {
 		int to;
 		double weight;
-		
+
 		public Link(int to, double weight) {
 			this.to = to;
 			this.weight = weight;
@@ -19,53 +22,90 @@ public class PathMaxProbaility {
 	}
 
 	public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
-		
+
 		HashMap<Integer, List<Link>> graph = new HashMap<Integer, List<Link>>();
-		//add all the nodes to the distance map and set the distance of the starting node to 0;
-		for(int i = 0; i < n; i++) {
-			if(start == i) {
-				dist.put(i, 1.0);
+		PriorityQueue<Link> pq = new PriorityQueue<>((a, b) -> Double.compare(a.weight, b.weight));
+
+		// build the graph
+		for (int i = 0; i < edges.length; i++) {
+			int[] edge = edges[i];
+			double weight = succProb[i];
+			if (!graph.containsKey(edge[0])) {
+				graph.put(edge[0], new ArrayList<Link>());
 			}
-			else {
+			if (!graph.containsKey(edge[1])) {
+				graph.put(edge[1], new ArrayList<Link>());
+			}
+			// undirected graph
+			graph.get(edge[0]).add(new Link(edge[1], weight));
+			graph.get(edge[1]).add(new Link(edge[0], weight));
+		}
+
+		Set<Integer> visited = new HashSet<>();
+		pq.add(new Link(start, 1));
+		while(!pq.isEmpty()) {
+			Link link = pq.poll();
+			int to = link.to;
+			double weight = link.weight;
+			if(to == end) return weight;
+			if(!visited.add(to))continue;
+			List<Link> children = graph.get(to);
+			for(Link child : children) {
+				pq.offer(new Link (child.to, child.weight * weight));
+			}
+			
+		}
+		return 0.0;
+	}
+
+	public double maxProbability2(int n, int[][] edges, double[] succProb, int start, int end) {
+
+		HashMap<Integer, List<Link>> graph = new HashMap<Integer, List<Link>>();
+		// add all the nodes to the distance map and set the distance of the starting
+		// node to 0;
+		for (int i = 0; i < n; i++) {
+			if (start == i) {
+				dist.put(i, 1.0);
+			} else {
 				dist.put(i, Double.MIN_VALUE);
 			}
 		}
-		
-		//build the graph
-		for(int i  = 0; i < edges.length; i++) {
+
+		// build the graph
+		for (int i = 0; i < edges.length; i++) {
 			int[] edge = edges[i];
 			double weight = succProb[i];
-			if(!graph.containsKey(edge[0])) {
+			if (!graph.containsKey(edge[0])) {
 				graph.put(edge[0], new ArrayList<Link>());
 			}
-			if(!graph.containsKey(edge[1])) {
+			if (!graph.containsKey(edge[1])) {
 				graph.put(edge[1], new ArrayList<Link>());
 			}
-			//undirected graph 
-			graph.get(edge[0]).add(new Link (edge[1], weight));
-			graph.get(edge[1]).add(new Link (edge[0], weight));
+			// undirected graph
+			graph.get(edge[0]).add(new Link(edge[1], weight));
+			graph.get(edge[1]).add(new Link(edge[0], weight));
 		}
-		
+
 		boolean[] seen = new boolean[n];
-		//start by picking the node with the shortest calculated distance
-		//at the start, it will be the "start" node itself
-		while(true) {
+		// start by picking the node with the shortest calculated distance
+		// at the start, it will be the "start" node itself
+		while (true) {
 			int candNode = -1;
 			double candDist = Double.MIN_VALUE;
-			for(int i = 0 ; i < n; i++) {
-				if(!seen[i] && dist.get(i) > candDist) {
+			for (int i = 0; i < n; i++) {
+				if (!seen[i] && dist.get(i) > candDist) {
 					candNode = i;
 					candDist = dist.get(i);
 				}
 			}
-			
-			if(candNode < 0) {
-				break; //this means there was a disconnected component
+
+			if (candNode < 0) {
+				break; // this means there was a disconnected component
 			}
 			seen[candNode] = true;
-			if(graph.containsKey(candNode)) {
+			if (graph.containsKey(candNode)) {
 				List<Link> children = graph.get(candNode);
-				for(Link child : children) {
+				for (Link child : children) {
 					dist.put(child.to, Math.max(dist.get(child.to), candDist * child.weight));
 				}
 			}
@@ -76,10 +116,10 @@ public class PathMaxProbaility {
 	public static void main(String[] args) {
 		PathMaxProbaility pmp = new PathMaxProbaility();
 		int n = 3;
-		int[][] edges = {{0,1},{1,2},{0,2}};
-		double[] succProb = {0.5,0.5,0.2};
+		int[][] edges = { { 0, 1 }, { 1, 2 }, { 0, 2 } };
+		double[] succProb = { 0.5, 0.5, 0.2 };
 		int start = 0;
-		int end =  2;
+		int end = 2;
 		System.out.println(pmp.maxProbability(n, edges, succProb, start, end));
 	}
 
