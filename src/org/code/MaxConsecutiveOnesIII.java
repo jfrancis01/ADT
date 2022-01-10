8b5da213c6400ed3d522b880314a380bee07467f
joel francis <joelfrancis@rogers.com>
2022-01-10 13:33:06 -0500

    cool algorithm

---
+++ src/org/code/MaxConsecutiveOnesIII.java
@@ -0,0 +1,31 @@
+package org.code;
+
+public class MaxConsecutiveOnesIII {
+	
+	public int longestOnes(int[] nums, int k) {
+		int left = 0, right;
+        for (right = 0; right < nums.length; right++) {
+            // If we included a zero in the window we reduce the value of k.
+            // Since k is the maximum zeros allowed in a window.
+            if (nums[right] == 0) {
+                k--;
+            }
+            // A negative k denotes we have consumed all allowed flips and window has
+            // more than allowed zeros, thus increment left pointer by 1 to keep the window size same.
+            if (k < 0) {
+                // If the left element to be thrown out is zero we increase k.
+                k += 1 - nums[left];
+                left++;
+            }
+        }     
+        return right - left;
+    }
+
+	public static void main(String[] args) {
+		int[] nums = {0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1};
+		MaxConsecutiveOnesIII mco3 = new MaxConsecutiveOnesIII();
+		System.out.println(mco3.longestOnes(nums, 2));
+		
+	}
+
+}
