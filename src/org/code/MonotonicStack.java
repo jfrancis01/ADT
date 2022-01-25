59bbf6fb6c5011a59a47f87e5cc7087700a71fef
joel francis <joelfrancis@rogers.com>
2022-01-24 21:00:18 -0500

    monotonic stack

---
+++ src/org/code/MonotonicStack.java
@@ -0,0 +1,26 @@
+package org.code;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+public class MonotonicStack {
+
+	public static void main(String[] args) {
+		int[] ints = {3,1,6,2,5,4};
+		MonotonicStack ms = new MonotonicStack();
+		List<Integer> nums = Arrays.stream(ints).boxed().toList();
+		ms.monoStack(nums);
+	}
+	
+	public void monoStack(List<Integer> nums) {
+		Stack<Integer> s = new Stack<Integer>();
+		for(int val : nums) {
+			while(!s.isEmpty() && s.get(s.size() - 1) <= val) {
+				s.remove(s.size() - 1);
+			}
+			s.add(val);
+		}
+	}
+}
