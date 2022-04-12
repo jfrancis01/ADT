c2df5ebd3b40ea1eb63ad9302802e76471da60e0
joel <joel@localhost>
2022-04-12 14:56:25 -0400

    tried and failed

--- src/org/code/DistinctSubsequence.java
+++ src/org/code/DistinctSubsequence.java
@@ -3,19 +3,23 @@ package org.code;
 public class DistinctSubsequence {
 
 	public static void main(String[] args) {
-		String  s = "rabbbit";
-		String t = "rabbit";
+		String  s = "babgbag";
+		String t = "bag";
 		DistinctSubsequence ds = new DistinctSubsequence();
 		System.out.println(ds.numDistinct(s, t));
 	}
 
 	public int numDistinct(String s, String t) {
 		int count = 0;
-		count  += helper(s, t, 0, "");
+		int[] cache = new int[s.length()];
+		for(int i = 0; i < cache.length; i++) {
+			cache[i] = -1;
+		}
+		count  += helper(s, t, 0, "", cache);
 		return count;
 	}
 	
-	public int helper(String s, String t, int i, String combo) {
+	public int helper(String s, String t, int i, String combo, int[] cache) {
 		
 		if(combo.equals(t)) {
 			return 1;
@@ -28,11 +32,14 @@ public class DistinctSubsequence {
 		if(i >= s.length()) {
 			return 0;
 		}
+		//if(cache[i] != -1) {
+		//	return cache[i];
+		//}
 		int with  = 0;
 		int without = 0;
-		with = helper(s, t, i + 1, combo + s.charAt(i));
-		without = helper(s, t, i + 1, combo);
-	
+		with = helper(s, t, i + 1, combo + s.charAt(i), cache);
+		without = helper(s, t, i + 1, combo, cache);
+		cache[i] = with + without;
 		return with + without;
 	}
 
