09745ed246a35aab8fb420dc6a42e6dc2756b1a1
joel francis <joelfrancis@rogers.com>
2022-07-02 12:23:28 -0400

    cheat checked in

---
+++ src/org/code/Interval.java
@@ -0,0 +1,14 @@
+package org.code;
+
+public class Interval {
+	public int start;
+	public int end;
+
+	public Interval() {
+	}
+
+	public Interval(int _start, int _end) {
+		start = _start;
+		end = _end;
+	}
+}
