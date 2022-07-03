e04d04da816af3f5d532d5635be654d4f8e729d4
joel francis <joelfrancis@rogers.com>
2022-07-02 20:39:12 -0400

    very well done!

--- src/org/code/EmployeeFreeTime.java
+++ src/org/code/EmployeeFreeTime.java
@@ -16,6 +16,7 @@ public class EmployeeFreeTime {
 				pq.offer(interval);
 			}
 		}
+		//print(pq);
 
 		if (pq.isEmpty())
 			return res;
@@ -35,6 +36,13 @@ public class EmployeeFreeTime {
 
 		return res;
 	}
+	
+	public void print(PriorityQueue<Interval> pq) {
+		while(!pq.isEmpty()) {
+			Interval interval = pq.poll();
+			System.out.print("[" + interval.start + ", " + interval.end + "], ");
+		}
+	}
 
 	public static void main(String[] args) {
 		EmployeeFreeTime eft = new EmployeeFreeTime();
@@ -43,6 +51,26 @@ public class EmployeeFreeTime {
 		Interval int2 = new Interval(6,7);
 		Interval int3 = new Interval(2,4);
 		Interval int4 = new Interval(2,5);
+		Interval int5 = new Interval(9,12);
+		
+		List<Interval> l1 = new ArrayList<Interval>();
+		l1.add(int1);
+		l1.add(int2);
+		List<Interval> l2 = new ArrayList<Interval>();
+		l2.add(int3);
+		List<Interval> l3 = new ArrayList<Interval>();
+		l3.add(int4);
+		l3.add(int5);
+		List<List<Interval>>schedule = new ArrayList<List<Interval>>();
+		schedule.add(l1);
+		schedule.add(l2);
+		schedule.add(l3);
+		
+		List<Interval> ans  = eft.employeeFreeTime(schedule);
+		for(Interval interval : ans) {
+			System.out.print("[" + interval.start + ", " + interval.end + "], ");
+		}
+		
 	}
 
 }
