0758ccd48971e4f29b7921a5cf14464e4e7f412c
sradmin <sradmin@10.0.0.70>
2024-04-26 22:31:46 -0400

    small error some where

--- src/org/code/MaxKSumPairs.java
+++ src/org/code/MaxKSumPairs.java
@@ -6,29 +6,42 @@ import java.util.HashMap;
 public class MaxKSumPairs {
 
 	public static void main(String[] args) {
-		int[] nums = {1, 2,3, 4};
-		int k = 5;
+		int[] nums = {2,2,2,3,1,1,4,1};
+		int k = 4;
 		MaxKSumPairs mksp = new MaxKSumPairs();
 		System.out.println(mksp.maxOperations(nums, k));
 	}
 
 	public int maxOperations(int[] nums, int k) {
-		ArrayList<Integer> list = new ArrayList<Integer>();
 		int ops = 0;
-		for(int i  = 0; i < nums.length; i++) {
-			list.add(nums[i]);
-		}
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		while()
+		HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
 		for(int i  = 0;  i < nums.length;  i++) {
-			int num = list.get(i);
 			int diff = k - nums[i];
 			if(map.containsKey(diff)) {
-				map.remove(diff);
-				list.remove(i);
-				ops++;
+				ArrayList<Integer> indices = map.get(diff);
+				if(indices.size()>0) {
+					indices.remove(0);
+					ops++;
+				}
+				else {
+					indices.add(i);
+					map.put(nums[i], indices);
+				}
+			}
+			else {
+				ArrayList<Integer> indexes = null;
+				if(map.containsKey(nums[i])) {
+					indexes = map.get(nums[i]);
+					indexes.add(i);
+				}
+				else {
+					indexes = new ArrayList<Integer>();
+					indexes.add(i);
+				}
+				map.put(nums[i], indexes);
 			}
 		}
+		return ops;
 	}
 
 }
