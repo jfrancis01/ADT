8e85d07ef2e36c20867ed3199e57706ce0c66bd5
sradmin <sradmin@sradmins-mbp.cgocable.net>
2024-05-01 05:29:21 -0400

    instead of using the index use the count as it was not asking you you to remove the numbers but only say how many pairs were removed

--- src/org/code/MaxKSumPairs.java
+++ src/org/code/MaxKSumPairs.java
@@ -6,42 +6,27 @@ import java.util.HashMap;
 public class MaxKSumPairs {
 
 	public static void main(String[] args) {
-		int[] nums = {2,2,2,3,1,1,4,1};
-		int k = 4;
+		int[] nums = {4,4,1,3,1,3,2,2,5,5,1,5,2,1,2,3,5,4};
+		int k = 2;
 		MaxKSumPairs mksp = new MaxKSumPairs();
 		System.out.println(mksp.maxOperations(nums, k));
 	}
 
 	public int maxOperations(int[] nums, int k) {
-		int ops = 0;
-		HashMap<Integer, ArrayList<Integer>> map = new HashMap<Integer, ArrayList<Integer>>();
-		for(int i  = 0;  i < nums.length;  i++) {
-			int diff = k - nums[i];
-			if(map.containsKey(diff)) {
-				ArrayList<Integer> indices = map.get(diff);
-				if(indices.size()>0) {
-					indices.remove(0);
-					ops++;
-				}
-				else {
-					indices.add(i);
-					map.put(nums[i], indices);
-				}
-			}
-			else {
-				ArrayList<Integer> indexes = null;
-				if(map.containsKey(nums[i])) {
-					indexes = map.get(nums[i]);
-					indexes.add(i);
-				}
-				else {
-					indexes = new ArrayList<Integer>();
-					indexes.add(i);
-				}
-				map.put(nums[i], indexes);
-			}
-		}
-		return ops;
+		 HashMap<Integer,Integer>map=new HashMap<>();
+	        int count=0;
+	        for(int i=0;i<nums.length;i++){
+	            //to check if that k-nums[i] present and had some value left or already paired
+	            if(map.containsKey(k-nums[i])&&map.get(k-nums[i])>0){
+	                count++;
+	                map.put(k-nums[i],map.get(k-nums[i])-1);
+	            }else{
+	                //getOrDefault is easy way it directly checks if value is 0 returns 0 where I added 1
+	                //and if some value is present then it return that value "similar to map.get(i)" and I added 1 on it 
+	                map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+	            }
+	        }
+	        return count;
 	}
 
 }
