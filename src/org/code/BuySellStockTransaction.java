f9edac95c1b6a59d27f57cec0da56962d3cf3657
sradmin <sradmin@sradmins-mbp.cgocable.net>
2024-06-25 22:27:52 -0400

    cached but only passes 34/44 cases

--- src/org/code/BuySellStockTransaction.java
+++ src/org/code/BuySellStockTransaction.java
@@ -1,20 +1,28 @@
 package org.code;
 
+import java.util.Arrays;
+
 public class BuySellStockTransaction {
 
 	public static void main(String[] args) {
-		int[] prices = { 1, 3, 2, 8, 4, 9 };
+		int[] prices = { 1,3,2,8,4,9 };
 		BuySellStockTransaction bsst = new BuySellStockTransaction();
 		System.out.println(bsst.maxProfit(prices, 2));
 	}
 
 	public int maxProfit(int[] prices, int fee) {
 		int start = 0;
-		return maxProfitHelper(prices, fee, start);
+		int size = prices.length;
+		int[]cache = new int[size + 1];
+		Arrays.fill(cache, -1);
+		return maxProfitHelper(prices, fee, start, cache);
 	}
 
-	private int maxProfitHelper(int[] prices, int fee, int start) {
+	private int maxProfitHelper(int[] prices, int fee, int start, int[]cache) {
 		int max = 0;
+		if(cache[start] != -1) {
+			return cache[start];
+		}
 		for (int i = start; i < prices.length; i++) {
 			int total = 0;
 			// buy here
@@ -23,11 +31,14 @@ public class BuySellStockTransaction {
 			total += -fee;
 			int profit;
 			for (int j = i + 1; j < prices.length; j++) {
-				profit = total + prices[j] + maxProfitHelper(prices, fee, j + 1);
+				//sell here and then do another transaction
+				profit = total + prices[j] + maxProfitHelper(prices, fee, j + 1, cache);
+				//save the maximum 
 				max = Math.max(profit, max);
 			}
 		}
-		return max;
+		cache[start] = max;
+		return cache[start];
 	}
 
 }
