29c13934f8ab69df4a5b723dff84aeb61a9eb239
jfrancis01 <joelfrancis@rogers.com>
2024-07-24 10:08:36 -0400

    brutal

--- src/org/code/MCOnesIII.java
+++ src/org/code/MCOnesIII.java
@@ -1,46 +1,31 @@
 package org.code;
 
-
 public class MCOnesIII {
 
 	public static void main(String[] args) {
 		MCOnesIII mcIII = new MCOnesIII();
-		int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+		int[] nums = { 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1};
 		int k = 2;
 		System.out.println(mcIII.longestOnes(nums, k));
 	}
-	
+
 	public int longestOnes(int[] nums, int k) {
-        int startPtr = 0;
-        int readPtr = 0;
-        int current = 0;
-        int max = Integer.MIN_VALUE;
-        int tempK = k;
-        while(startPtr < nums.length){
-        	if(nums[readPtr] == 1) {
-        		current++;
-        		readPtr++;
-        	}
-        	else {
-        		if(tempK > 0) {
-        			current++;
-        			readPtr++;
-        			tempK--;
-        		}
-        		else {
-        			max = Math.max(max, current);
-        			current = 0;
-        			tempK = k;
-        			if(readPtr == startPtr) {
-        				startPtr++;
-        			}
-        			else {
-        				startPtr = readPtr;
-        			}
-        		}
-        	}
-        }
-        return max;
-    }
+		int max = 0;
+		int left = 0;
+		int zeros = 0;
+		for (int right = 0; right < nums.length; right++) {
+			if (nums[right] == 0) {
+				zeros++;
+			}
+			while (zeros > k) {
+				if (nums[left] == 0) {
+					zeros--;
+				}
+				left++;
+			}
+			max = Math.max(max, right - left + 1);
+		}
+		return max;
+	}
 
 }
