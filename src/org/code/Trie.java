bc3847eae000ddaa72c544e37fddb45b26d929d3
sradmin <sradmin@sradmins-mbp.cgocable.net>
2024-07-29 23:42:35 -0400

    solved by my self but tricky

--- src/org/code/Trie.java
+++ src/org/code/Trie.java
@@ -21,30 +21,65 @@ public class Trie {
 	}
 
 	public void insert(String word) {
-
-		for(int i  = 0; i < word.length(); i++) {
-			boolean last = i == word.length()-1? true:false;
-			insertHelper(word.charAt(i), root, last);
-		}
+		insertHelper(word, 0, this.root);
 	}
 	
-	private void insertHelper(Character c , TrieNode node, boolean last) {
-		if(node.end == false) {
+	private void insertHelper(String word, int index , TrieNode node) {
+		if(index >= word.length()) {
+			node.end = true;
 			return;
 		}
-		
+		if(!node.children.containsKey(word.charAt(index))) {
+			TrieNode newNode = new TrieNode();
+			newNode.children = new HashMap<Character, TrieNode>();
+			if(index == word.length() - 1) {
+				newNode.end = true;
+			}
+			node.children.put(word.charAt(index), newNode);
+		}
+		insertHelper(word, index + 1, node.children.get(word.charAt(index)));
 	}
+	
 	public boolean search(String word) {
-		return false;
+		return searchHelper(word, 0, this.root);
+	}
+	
+	private boolean searchHelper(String word, int index, TrieNode node) {
+		if(index >= word.length()) {
+			return node.end;
+		}
+		if(node.children.containsKey(word.charAt(index))) {
+			return searchHelper(word, index + 1, node.children.get(word.charAt(index)));
+		}
+		else {
+			return false;
+		}
 	}
 
 	public boolean startsWith(String prefix) {
-		return false;
+		return startsWithHelper(prefix, 0, this.root);
+	}
+	
+	private boolean startsWithHelper(String prefix, int index, TrieNode node) {
+		if(index >= prefix.length()) {
+			return true;
+		}
+		if(node.children.containsKey(prefix.charAt(index))) {
+			return startsWithHelper(prefix, index + 1, node.children.get(prefix.charAt(index)));
+		}
+		else {
+			return false;
+		}
 	}
 
 	public static void main(String[] args) {
 		Trie trie = new Trie();
-		trie.insert("abc");
+		trie.insert("apple");
+		System.out.println(trie.search("apple"));
+		System.out.println(trie.search("app"));
+		System.out.println(trie.startsWith("app"));
+		trie.insert("app");
+		System.out.println(trie.search("app"));
 	}
 
 }
